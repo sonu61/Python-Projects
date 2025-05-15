5626a55d4aa2abe6971432b837331160d855aaf8
sonu61 <106292314+sonu61@users.noreply.github.com>
2025-05-15 16:37:32 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,4 @@
-# class User:
-#     def __init__(self,user_id,user_name):
-#         print('new user being created...')
-#         self.user_id=user_id
-#         self.user_name = user_name
-#         self.followers =0
-#         self.following=0
-#
-#     def follow(self,user):
-#         user.followers+=1
-#         self.following+=1
-#
-#
-#
-# user_1=User('12','Sonu')
-# user_2=User('11','samit')
-# user_2.follow(user_1)
-#
-# print(user_1.followers)
-# print(user_1.following)
-#
-#
-# print(user_2.followers)
-# print(user_2.following)
+
 from day17.question_model import Question
 from day17.data import question_data
 from day17.quiz_brain import QuizBrain
